192873c6827bb276aec1780440b7f101e9ba8f02
antonio.agostino <antonio.agostino@accenture.com>
2024-05-13 09:38:25 +0200

    cristo

--- vars/utilities.groovy
+++ vars/utilities.groovy
@@ -3,10 +3,24 @@ import java.util.Scanner;
 import java.io.FileNotFoundException;
 
 
-def file (String testo) {
-    def newFile = new File("C:/Work File/bro.txt")
-    newFile.createNewFile()
-    newFile.append(testo) 
-    echo('file scaricato') 
-    return file
+def txtFile (String testo) {
+
+    def filePath = "C:/Work File/bro.txt"
+
+    try {
+        
+        def file = new File(filePath)
+
+        
+        def content = testo
+
+        
+        file.write(content)
+
+        echo("File creato e scritto correttamente.")
+    } catch (FileWritingException e) {
+        echo("Errore durante la scrittura del file: ${e.message}")
+    } catch (Exception e) {
+        echo("Errore generico: ${e.message}")
+    }
 }
