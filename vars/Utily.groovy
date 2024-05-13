8b3d6049aeae4c33edc4fc28360f4c3f49107a9b
antonio.agostino <antonio.agostino@accenture.com>
2024-05-13 10:10:49 +0200

    miao

---
+++ vars/Utily.groovy
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
+public Class Utily {
+
+
+    Utily(){}  
+
+    def txtFile (String testo) {
+
+        def filePath = "C:/Work File/bro.txt"
+
+        try {
+            
+            def file = new File(filePath)
+            file.write(testo)
+
+            echo("File creato e scritto correttamente.")
+        } catch (Exception e) {
+            echo("Errore durante la scrittura del file: ${e.message}")
+        } catch (Exception e) {
+            echo("Errore generico: ${e.message}")
+        }
+    }
+
+
+}
+
