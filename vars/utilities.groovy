e93c331f6a03c74f7b0b9a44b290ad9447205dad
antonio.agostino <antonio.agostino@accenture.com>
2024-05-13 09:34:11 +0200

    s

--- vars/utilities.groovy
+++ vars/utilities.groovy
@@ -3,9 +3,10 @@ import java.util.Scanner;
 import java.io.FileNotFoundException;
 
 
-def file(String testo) {
+def file (String testo) {
     def newFile = new File("C:/Work File/bro.txt")
     newFile.createNewFile()
     newFile.append(testo) 
     echo('file scaricato') 
+    return file
 }
